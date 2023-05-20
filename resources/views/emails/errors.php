0399e15fc2de778f40ed5bf3f7184a1b64853773
0xkhan <0xkhan@protonmail.com>
2023-05-20 12:24:57 +0200

    Add email templates

---
+++ resources/views/emails/errors.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <div style="width: 60%; padding: 15px; margin 0 auto; background-color: #666; color: #fff;">
+        <?php echo "Error {$data}"; ?>
+
+        <p>
+            Sent from the website!<br/><br/>
+            <strong>There are errors, fix them!</strong>
+        </p>
+    </div>
+</body>
+</html>
