4807db6622329f149bee075287dfb3b3e933b340
0xkhan <0xkhan@protonmail.com>
2023-05-20 12:34:11 +0200

    Update routes.php

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -11,3 +11,33 @@ $router->map(
     'home'
 );
 
+
+// Admin routes
+$router->map(
+    'GET',
+    '/admin',
+    'App\Controllers\Admin\DashboardController@show',
+    'admin_dashboard'
+);
+
+$router->map(
+    'POST',
+    '/admin',
+    'App\Controllers\Admin\DashboardController@get',
+    'admin_form'
+);
+
+// Product management
+$router->map(
+    'GET',
+    '/admin/product/categories',
+    'App\Controllers\Admin\ProductCategoryController@show',
+    'product_category'
+);
+
+$router->map(
+    'POST',
+    '/admin/product/categories',
+    'App\Controllers\Admin\ProductCategoryController@store',
+    'create_product_category'
+);
