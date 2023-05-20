18eaf4fac75c21bc684baa75ab3b77130af0088c
0xkhan <0xkhan@protonmail.com>
2023-05-20 12:32:45 +0200

    Update IndexController.php

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,12 +2,29 @@
 
 namespace App\Controllers;
 
+use App\Classes\Mail;
+
 class IndexController extends BaseController
 {
 
     public function show()
     {
         echo "Inside Homepage from IndexController";
+
+        // $mail = new Mail();
+        // $data = [
+        //     'to' => 'test@example.com',
+        //     'subject' => 'Welcome to store',
+        //     'view' => 'welcome', // view file name
+        //     'name' => 'John Doe',
+        //     'body' => 'Testing email template'
+        // ];
+
+        // if ($mail->send($data)) {
+        //     echo 'email sent successfully!';
+        // } else {
+        //     echo 'email sending failed';
+        // }
     }
 
 }
