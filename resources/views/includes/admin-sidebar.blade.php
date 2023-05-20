c50550fa68a0602f227597acba122479e1306ab3
0xkhan <0xkhan@protonmail.com>
2023-05-20 12:26:17 +0200

    Add includes

---
+++ resources/views/includes/admin-sidebar.blade.php
@@ -0,0 +1,25 @@
+<!--<div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas-content>-->
+<div class="off-canvas position-left reveal-for-large" id="offCanvas" data-off-canvas-content>
+
+    <h3>Welcome Admin</h3>
+
+    <div>
+        <img src="/images/avatar.png" alt="Avatar" title="Admin">
+        <p>Khan</p>
+    </div>
+    
+    <!-- Side Bar -->
+    <ul class="vertical menu">
+      <li><a href="/admin"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#127899;</span>Dashboard</a></li>
+      <li><a href="/admin/users"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#128106;</span>Users</a></li>
+      <li><a href="/admin/product/create"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#10133;</span>Add Product</a></li>
+      <li><a href="/admin/products"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#128188;</span>Manage Products</a></li>
+      <li><a href="/admin/product/categories"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#128451;</span>Categories</a></li>
+      <li><a href="/admin/transactions/orders"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#128722;</span>View Orders</a></li>
+      <li><a href="/admin/transactions/payments"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#128179;</span>Payments</a></li>
+      <li><a href="/logout"><span style="font-size:18px;font-weight:600;margin-right:7px;">&#127770;</span>Logout</a></li>
+    </ul>
+    <!-- End Side Bar -->
+    
+</div>
+
