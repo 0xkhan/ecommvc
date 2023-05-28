b185d4917db693b1378b4c0a325b1750b0a1f28b
0xkhan <0xkhan@protonmail.com>
2023-05-28 11:29:59 +0200

    Update app.js - Instantiate SassyModal

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,14 @@
+import SassyModal from './plugins/SassyModal';
+
 // Custon Js Files
 require('../../assets/js/ecommvc');
 require('../../assets/js/front/navbar');
+require('../../assets/js/admin/update');
 require('../../assets/js/init');
+
+const sassyModal = new SassyModal({
+    blur: true,
+    centered: true,
+    animation: 'fade-in',
+    showModalCSS: 'show-modal'
+});
