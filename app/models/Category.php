720ee2111cccfc79b4488d0f5c68aeece224486c
0xkhan <0xkhan@protonmail.com>
2023-05-28 10:46:28 +0200

    Update Category.php - Add transform method to transform retrieved data about categories into an array

--- app/models/Category.php
+++ app/models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Category extends Model
 {
@@ -11,8 +12,24 @@ class Category extends Model
     use SoftDeletes;
 
     public $timestamps = true;
-    protected $dateFormat = 'Y-m-d H:i:s.u';
     protected $fillable = ['name', 'slug'];
     protected $dates = ['deleted_at'];
+
+    public function transform($data)
+    {
+        $categories = [];
+
+        foreach($data as $item) {
+            $added = new Carbon($item->created_at);
+            array_push($categories, [
+                'id' => $item->id,
+                'name' => $item->name,
+                'slug' => $item->slug,
+                'added' => $added->toFormattedDateString()
+            ]);
+        }
+
+        return $categories;
+    }
 
 }
