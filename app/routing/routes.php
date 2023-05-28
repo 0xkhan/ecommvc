4217e1eb51870bff14afdbb929a2883a18a1b141
0xkhan <0xkhan@protonmail.com>
2023-05-28 10:46:48 +0200

    Update routes.php

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -41,3 +41,17 @@ $router->map(
     'App\Controllers\Admin\ProductCategoryController@store',
     'create_product_category'
 );
+
+$router->map(
+    'POST',
+    '/admin/product/categories/[i:id]/edit',
+    'App\Controllers\Admin\ProductCategoryController@edit',
+    'edit_product_category'
+);
+
+$router->map(
+    'POST',
+    '/admin/product/categories/[i:id]/delete',
+    'App\Controllers\Admin\ProductCategoryController@delete',
+    'delete_product_category'
+);
